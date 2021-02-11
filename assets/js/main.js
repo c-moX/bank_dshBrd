b3d3ca39e34f1957568a4b8687f15e5c7530a693
c-moX <cmoxsh@gmail.com>
2021-02-11 19:39:29 +0600

    main.js file add function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // Display Login Area
-const logInBtn = document.getElementById("login-btn").addEventListener("click", function(){
+const logInBtn = document.getElementById("login-btn").addEventListener("click", function () {
     const logInArea = document.getElementById("login-area");
     logInArea.style.display = "none";
 
@@ -9,34 +9,44 @@ const logInBtn = document.getElementById("login-btn").addEventListener("click", 
 
 //Deposit and Balance button event handler 01
 const depositBtn = document.getElementById("addDeposit");
-depositBtn.addEventListener("click", function(){
+depositBtn.addEventListener("click", function () {
     const depositNumber = getInputNumber("depositAmount");
 
-    updateDepositAndBalance("currentDeposit", depositNumber);
-    updateDepositAndBalance("currentBalance", depositNumber);
+    if (depositNumber < 0) {
+        alert("Deposit number can't be nagative.");
+    }
+    else {
+        updateDepositAndBalance("currentDeposit", depositNumber);
+        updateDepositAndBalance("currentBalance", depositNumber);
 
-    document.getElementById("depositAmount").value = ""; //For deposit placeholder empty
+        document.getElementById("depositAmount").value = ""; //For deposit placeholder empty
+    }
 })
 
 //Withdraw and Balance button event handler 02
 const withdrawtBtn = document.getElementById("addWithdraw");
-withdrawtBtn.addEventListener("click", function(){
+withdrawtBtn.addEventListener("click", function () {
     const withdrawNumber = getInputNumber("withdrawAmount");
-    
-    updateDepositAndBalance("currentWithdraw", withdrawNumber);
-    updateDepositAndBalance("currentBalance", -1 * withdrawNumber); //For negative number (-1 * num)
 
-    document.getElementById("withdrawAmount").value = ""; //For withdraw placeholder empty
+    if (withdrawNumber < 0) {
+        alert("Withdraw number can't be negative.");
+    }
+    else {
+        updateDepositAndBalance("currentWithdraw", withdrawNumber);
+        updateDepositAndBalance("currentBalance", -1 * withdrawNumber); //For negative number (-1 * num)
+
+        document.getElementById("withdrawAmount").value = ""; //For withdraw placeholder empty
+    }
 })
 
 //Deposit and Withdraw input number function
-function getInputNumber(id){
+function getInputNumber(id) {
     const amount = document.getElementById(id).value;
     const amountNumber = parseFloat(amount);
     return amountNumber;
 }
 //Deposit and Balance updated function
-function updateDepositAndBalance(id, addedNumber){
+function updateDepositAndBalance(id, addedNumber) {
     const current = document.getElementById(id).innerText;
     const currentNumber = parseFloat(current);
     const totalAmount = addedNumber + currentNumber;
